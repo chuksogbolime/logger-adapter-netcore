98097a53b65fddc018c4d7c1ee3053b8ded7a8ec
chuksogbolime <chuksogbolime@gmail.com>
2020-09-24 11:32:55 +0100

    Initial commit

---
+++ NetCoreLoggerAdapter.Tests/LoggerServiceAdapterTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NetCoreLoggerAdapter.Tests
+{
+    public class LoggerServiceAdapterTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ NetCoreLoggerAdapter/LoggerServiceAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace NetCoreLoggerAdapter
+{
+    public class LoggerServiceAdapter<T> : ILoggerServiceAdapter<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerServiceAdapter(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogError(Exception ex, string message, params object[] args)
+        {
+            _logger.LogError(ex, message, args);
+        }
+
+        public void LogInformation(string message)
+        {
+            _logger.LogInformation(message);
+        }
+    }
+}
